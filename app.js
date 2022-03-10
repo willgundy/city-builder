cf00559ee038cec476ef78c5ff05f52316da67f4
Will Gundy <willgunderson503@gmail.com>
2022-03-10 14:43:13 -0800

    added all element selectors

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 // import functions and grab DOM elements
+//get containers
+const imageContainerEl = document.getElementById('imageContainer');
+const selectorContainerEl = document.getElementById('selectorContainer');
+const textContainerEl = document.getElementById('textContainer');
+
+//get images
+const regionImageEl = document.getElementById('region-image');
+const typeImageEl = document.getElementById('type-image');
+const sizeImageEl = document.getElementById('size-image');
+
+//get selectors
+const regionSelectorEl = document.getElementById('region');
+const typeSelectorEl = document.getElementById('type');
+const sizeSelectorEl = document.getElementById('size');
+
+//get text and input elements
+const sloganInputEl = document.getElementById('slogan-input');
+const sloganTextEl = document.getElementById('sloganText');
+const changeCounterTextEl = document.getElementById('changeCounter');
 
 // let state
+let regionChangeCount = 0;
+let typeChangeCount = 0;
+let sizeChangeCount = 0;
 
 // set event listeners 
   // get user input
